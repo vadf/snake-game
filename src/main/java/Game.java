fe3a5758e689faf1cf54cc2476f9d49fea681b06
Vadim <vad.filippov@gmail.com>
2013-11-09 17:30:05 +0200

    Update GUI part.
    Change-Id: I4620695f8d47f77f958994284137b9c566a80b8c

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,8 +5,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -14,73 +18,87 @@ import javax.swing.Timer;
 
 public class Game {
 
-    private static int       defaultSnakeSize      = 3;
-    private static Direction defaultSnakeDirection = Direction.RIGHT;
-    private static TextPoint defaultSnakeHead      = new TextPoint(1, 5);
-    private static String    defaultField          = "src/main/resources/defaultField.txt";
-    private static int       defaultNumOfStars     = 3;
-    private static TextPoint defaultP1             = new TextPoint(1, 1);
-    private static Direction defaultD1             = Direction.RIGHT;
-    private static TextPoint defaultP2             = new TextPoint(9, 17);
-    private static Direction defaultD2             = Direction.UP;
+    static int                   defaultSnakeSize      = 3;
+    static Direction             defaultSnakeDirection = Direction.RIGHT;
+    static TextPoint             defaultSnakeHead      = new TextPoint(1, 5);
+    static String                defaultField          = "src/main/resources/defaultField.txt";
+    static int                   defaultNumOfStars     = 3;
+    static TextPoint             defaultP1             = new TextPoint(1, 1);
+    static Direction             defaultD1             = Direction.RIGHT;
+    static TextPoint             defaultP2             = new TextPoint(9, 17);
+    static Direction             defaultD2             = Direction.UP;
 
-    public static String     textField;
-    public static int        key                   = 0;
-    public static boolean    move                  = false;
-    public static int        score                 = 0;
+    public static String         textField;
+    public static int            key                   = 0;
+    public static boolean        move                  = false;
+    public static int            score                 = 0;
+
+    public static GameController game;
+    public static TextPoint      fieldSize;
+    public static JFrame         frame;
+    public static Timer          t                     = new Timer(1000,
+                                                               new MoveTask());
 
     public static void main(String[] args) {
-        Timer t = new Timer(1000, new MoveTask());
-        GameController game = new GameController();
-        try {
-            final TextPoint fieldSize = game.initField(defaultField);
-            game.initSnake(defaultSnakeHead, defaultSnakeDirection,
-                    defaultSnakeSize);
-            game.initPorts(defaultP1, defaultD1, defaultP2, defaultD2);
-            game.addStars(defaultNumOfStars);
 
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    initGUI(fieldSize.row, fieldSize.col);
-                }
-            });
-
-            t.start();
-            while (true) {
-                textField = game.toString();
-
-                // read key and turn
-                game.setDirection(key);
-                key = 0;
-
-                // move snake and check
-                if (move) {
-                    game.move();
-                    move = false;
-                    score = game.getScore();
-                }
-
-                // generate new star if needed
-                if (game.checkAndCreateStar()) {
-                    System.out.println("Congratulations");
-                    break;
-                }
+        game = new GameController();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                loadGame();
             }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-        t.stop();
+        });
+
     }
 
-    public static void initGUI(int rows, int cols) {
-        JFrame frame = new JFrame("Snake Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+    public static void initGameField(int rows, int cols) {
         frame.add(new GameArea(rows, cols), BorderLayout.CENTER);
         frame.add(new ScorePanel(cols), BorderLayout.SOUTH);
         frame.pack();
-        frame.setVisible(true);
+        t.start();
     }
+
+    public static void loadGame() {
+        frame = new JFrame("Snake Game Loader");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel pLoad = new JPanel();
+        pLoad.setLayout(new BoxLayout(pLoad, BoxLayout.Y_AXIS));
+
+        JButton btnDefault = new JButton("Default Game");
+        JButton btnGame = new JButton("New Game");
+
+        btnDefault.setBounds(0, 0, 40, 10);
+        btnGame.setBounds(0, 0, 40, 10);
+        btnDefault.addActionListener(new DefaultGame());
+
+        pLoad.add(btnDefault);
+        pLoad.add(btnGame);
+        frame.add(pLoad, BorderLayout.EAST);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
+
+class DefaultGame implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            Game.fieldSize = Game.game.initField(Game.defaultField);
+            Game.game.initSnake(Game.defaultSnakeHead,
+                    Game.defaultSnakeDirection, Game.defaultSnakeSize);
+            Game.game.initPorts(Game.defaultP1, Game.defaultD1, Game.defaultP2,
+                    Game.defaultD2);
+            Game.game.addStars(Game.defaultNumOfStars);
+            Game.initGameField(Game.fieldSize.row, Game.fieldSize.col);
+        } catch (IOException | OutOfFieldException | SnakeOnWallException
+                | TeleportInitException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+    }
+
 }
 
 class GameArea extends JTextArea implements KeyListener {
@@ -108,7 +126,7 @@ class GameArea extends JTextArea implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        Game.key = e.getKeyCode();
+        Game.game.setDirection(e.getKeyCode());
     }
 
     @Override
@@ -138,6 +156,20 @@ class MoveTask implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Game.move = true;
+
+        try {
+            Game.game.move();
+            // generate new star if needed
+            if (Game.game.checkAndCreateStar()) {
+                System.out.println("Congratulations");
+                Game.t.stop();
+            }
+        } catch (SnakeOnWallException | OutOfFieldException | SnakeAddException
+                | SnakeCollision e1) {
+            e1.printStackTrace();
+            Game.t.stop();
+        }
+        Game.score = Game.game.getScore();
+        Game.textField = Game.game.toString();
     }
 }
